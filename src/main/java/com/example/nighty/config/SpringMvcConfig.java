0ecb1f96a1b5b5fbea509d21cb402e449d977c7b
littlelittleJessica <446067382@qq.com>
2021-10-13 19:04:01 +0800

    增加登录拦截器

---
+++ src/main/java/com/example/nighty/config/SpringMvcConfig.java
@@ -0,0 +1,24 @@
+package com.example.nighty.config;
+
+import com.example.nighty.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/user/login"
+                );
+    }
+}
+
